ec7f619979e37fcd67d6f89a5330f9c89a1abe1b
Christian Schuetz <christian@geoffrey>
2014-11-11 00:16:15 -0600

    Changed some stuff

--- spammer.js
+++ spammer.js
@@ -6,6 +6,29 @@ var courses = {}
 var crn = ''
 
 // Load the page from localhost
+var parse = function(){
+  var $ = cheerio.load(browser.html());
+  $("tr","table.datadisplaytable").each(function(){
+    $(this).find(".dddefault").each(function(){
+      $(this).find("input").each(function() {
+        if($(this).attr("name") == "CRN_IN"){
+          crn = $(this).attr("value");
+          courses[crn] = {}
+        }
+        if($(this).attr("name") == "SUBJ"){
+          courses[crn].subject = $(this).attr("value");
+        }
+        if($(this).attr("name") == "CRSE"){
+          courses[crn].course = $(this).attr("value");
+        }
+        if($(this).attr("name") == "TITLE"){
+          courses[crn].title = $(this).attr("value");
+        }
+      });
+    })
+  });
+}
+
 browser = new Browser()
 browser.
   visit("https://eas.admin.uillinois.edu/eas/servlet/EasLogin?redirect=https://webprod.admin.uillinois.edu/ssa/servlet/SelfServiceLogin?appName=edu.uillinois.aits.SelfServiceLogin&dad=BANPROD1").
@@ -16,23 +39,8 @@ browser.
   then(function() {return browser.clickLink("I Agree to the Above Statement")}).
   then(function() {return browser.pressButton("Submit")}).
   then(function() {
-    var $ = cheerio.load(browser.html());
-    $("tr","table.datadisplaytable").each(function(){
-      $(this).find(".dddefault").each(function(){
-        $(this).find("input").each(function() {
-          if($(this).attr("name") == "CRN_IN"){
-            crn = $(this).attr("value");
-            courses[crn] = {}
-          }
-          if($(this).attr("name") == "SUBJ"){
-            courses[crn].subject = $(this).attr("value");
-          }
-          if($(this).attr("name") == "CRSE"){
-            courses[crn].course = $(this).attr("value");
-          }
-        });
-      })
-      console.log(courses)
-    });
-
+    return parse();
+  }).
+  then(function(){
+    console.log(courses)
   });
