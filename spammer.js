8c2da01a1f2d3a41832959ca7eb9ae1eeb6305c2
cmschuetz <cmschuetz12@gmail.com>
2014-11-17 22:51:06 -0600

    Got async while loop to work

--- spammer.js
+++ spammer.js
@@ -1,33 +1,11 @@
+var async = require("async");
 var Browser = require("zombie");
 var assert = require("assert");
 var cheerio = require("cheerio")
 var config = require("./config.json")
 var courses = {}
+var toAdd = ['cool','dix','d','s']
 var crn = ''
-
-// Load the page from localhost
-var parse = function(){
-  var $ = cheerio.load(browser.html());
-  $("tr","table.datadisplaytable").each(function(){
-    $(this).find(".dddefault").each(function(){
-      $(this).find("input").each(function() {
-        if($(this).attr("name") == "CRN_IN"){
-          crn = $(this).attr("value");
-          courses[crn] = {}
-        }
-        if($(this).attr("name") == "SUBJ"){
-          courses[crn].subject = $(this).attr("value");
-        }
-        if($(this).attr("name") == "CRSE"){
-          courses[crn].course = $(this).attr("value");
-        }
-        if($(this).attr("name") == "TITLE"){
-          courses[crn].title = $(this).attr("value");
-        }
-      });
-    })
-  });
-}
 
 browser = new Browser()
 browser.
@@ -39,8 +17,38 @@ browser.
   then(function() {return browser.clickLink("I Agree to the Above Statement")}).
   then(function() {return browser.pressButton("Submit")}).
   then(function() {
-    return parse();
+    async.whilst(
+      function() {return toAdd.length != 0},
+      function(callback){
+        var $ = cheerio.load(browser.html());
+        $("tr","table.datadisplaytable").each(function(){
+          $(this).find(".dddefault").each(function(){
+            $(this).find("input").each(function() {
+              if($(this).attr("name") == "CRN_IN"){
+                crn = $(this).attr("value");
+                courses[crn] = {}
+              }
+              if($(this).attr("name") == "SUBJ"){
+                courses[crn].subject = $(this).attr("value");
+              }
+              if($(this).attr("name") == "CRSE"){
+                courses[crn].course = $(this).attr("value");
+              }
+              if($(this).attr("name") == "TITLE"){
+                courses[crn].title = $(this).attr("value");
+              }
+            });
+          })
+        });
+        console.log(courses)
+        toAdd.pop();
+        browser.pressButton("Submit Changes").then(callback);
+     },
+     function(err){
+       console.log("done");
+     });
+
   }).
   then(function(){
-    console.log(courses)
+    //console.log(courses)
   });
