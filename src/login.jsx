2bc26974a06fd1922053cb1ca80450418ac130ee
cmschuetz <cmschuetz12@gmail.com>
2015-01-08 00:45:43 -0600

    Deleted some stuff

--- src/login.jsx
+++ src/login.jsx
@@ -1,9 +1,6 @@
 var React = require('react')
 var Navigation = require('react-router').Navigation;
-var async = require('async')
-var Courses = require('./courses.jsx')
-var Animations = require('./animations.jsx')
-var router = require('./router.jsx')
+
 module.exports = React.createClass({
 
   mixins: [Navigation],
